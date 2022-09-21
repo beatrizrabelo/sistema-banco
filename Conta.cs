610613ff25b5c4ee2dba2338f5e87c4197637472
Beatriz Rabelo <beatriz.devrabelo@gmail.com>
2022-09-20 23:27:28 -0300

    Adicionando metodos em Conta

--- Conta.cs
+++ Conta.cs
@@ -8,9 +8,58 @@ namespace SistemaBanco
     public class Conta
     {
         public Usuario Usuario { get; set; }
-        public int Agencia { get;}
-        public int Numero { get;}
+        public int Agencia { get; }
+        public int Numero { get; }
         private double _saldo;
 
+        public Conta(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+        }
+
+        public void consultarSaldo()
+        {
+            Console.WriteLine("Saldo: R$ " + _saldo);
+        }
+
+        public void sacarSaldo(double valor)
+        {
+            if (valor < 0)
+            {
+                Console.WriteLine("Valor inválido para o saque.");
+            }
+
+            if (_saldo < valor)
+            {
+                Console.WriteLine("Saldo indiponivel para saque.");
+            }
+
+            Console.WriteLine("Valor sacado de: R$ " + valor);
+            _saldo -= valor;
+        }
+
+        public void depositarValor(double valor)
+        {
+            if (valor < 0)
+            {
+                Console.WriteLine("Valor inválido para o deposito.");
+            }
+
+            Console.WriteLine("Valor depositado de R$ " + valor);
+            _saldo += valor;
+        }
+
+        public void transferirValor(double valor, Conta contaDestino)
+        {
+            if (valor < 0)
+            {
+                Console.WriteLine("Valor inválido para a transferência.");
+            }
+
+            sacarSaldo(valor);
+
+            contaDestino.depositarValor(valor);
+        }
     }
 }
